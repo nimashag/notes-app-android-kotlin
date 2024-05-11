5fa32919e266678fee987b340462295fbd39d94c
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-05-11 09:11:53 +0530

    Set the Room database

---
+++ app/src/main/java/com/example/notes_app_kotlin/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.notes_app_kotlin.repository
+
+import com.example.notes_app_kotlin.database.NoteDatabase
+import com.example.notes_app_kotlin.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+}
