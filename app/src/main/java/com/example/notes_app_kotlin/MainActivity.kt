8feb40f37d0bcbf4ef93cd1b3cc171c0d759bf25
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-05-11 17:31:34 +0530

    Set-up ViewModel and Adapter

--- app/src/main/java/com/example/notes_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/notes_app_kotlin/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.notes_app_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.notes_app_kotlin.database.NoteDatabase
+import com.example.notes_app_kotlin.repository.NoteRepository
+import com.example.notes_app_kotlin.viewmodel.NoteViewModel
+import com.example.notes_app_kotlin.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+
     }
 }
